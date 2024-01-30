1b4e4af569ed740778029ae7cfea1eb6300f9557
margaux.delaf <margaux.delfosse@edu.devinci.fr>
2024-01-30 10:57:39 +0100

    feat: setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+name='quiz_game_MargauxDelafosse_CDFO2',
+version='1.0',
+author='{margauxdelaf}',
+license='MIT',
+long_description=open('README.md').read(),
+)
